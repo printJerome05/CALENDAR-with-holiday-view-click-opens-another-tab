47d034160c40888e01b4a6bdb76d6dbb63d5f586
Jerome Marco <jerome.marco.pixel8@gmail.com>
2023-07-22 12:59:11 +0800

    first commit

---
+++ holidayscript.js
@@ -0,0 +1,55 @@
+var newWindow;
+var w, h = 0;
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+function holidayFIC(){
+newWindow = window.open("fic.html", "fic", "width=640, height=480");
+w = width / 2;
+h = height / 2;
+newWindow.moveBy(w-300,h-200);
+newWindow.focus();
+}
+
+
+
+function holidaySND(){
+newWindow = window.open("snd.html", "snd", "width=640, height=480");
+w = width / 2;
+h = height / 2;
+newWindow.moveBy(w-300,h-200);
+newWindow.focus();
+}
+
+function holidayCE(){
+newWindow = window.open("ce.html", "ce", "width=640, height=480");
+w = width / 2;
+h = height / 2;
+newWindow.moveBy(w-300,h-200);
+newWindow.focus();
+}
+
+function holidayCD(){
+newWindow = window.open("cd.html", "cd", "width=640, height=480");
+w = width / 2;
+h = height / 2;
+newWindow.moveBy(w-300,h-200);
+newWindow.focus();
+}
+
+
+function holidayRD(){
+newWindow = window.open("rd.html", "rd", "width=640, height=480");
+w = width / 2;
+h = height / 2;
+newWindow.moveBy(w-300,h-200);
+newWindow.focus();
+}
+
+function holidayNYE(){
+newWindow = window.open("nye.html", "nye", "width=640, height=480");
+w = width / 2;
+h = height / 2;
+newWindow.moveBy(w-300,h-200);
+newWindow.focus();
+}
